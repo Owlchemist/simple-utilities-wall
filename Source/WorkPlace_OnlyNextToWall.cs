ef2dfcb63f01aab2e596714b869e73b6cfb0972c
Owlchemist <owlchemist@protonmail.com>
2021-08-17 17:15:51 -0700

    Fix placement with smooth stone walls

--- Source/WorkPlace_OnlyNextToWall.cs
+++ Source/WorkPlace_OnlyNextToWall.cs
@@ -9,28 +9,26 @@ namespace WallUtilities
 	{
 		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
 		{
-			//check roof
+			//Check roof
 			if (map.roofGrid.Roofed(loc) == false) return new AcceptanceReport("Space in front of heater must be under a roof.");
 
 			//check walls/doors or other heaters
 			Building building = map.thingGrid.ThingAt<Building>(loc);
 			if (building != null && (building.def.fillPercent == 1f || building.def.thingClass == typeof(Building_Heater))) return new AcceptanceReport("Space in front of heater must not be a wall, door, or other heater.");
-			//check blueprint
+
+			//Check blueprint
 			if (map.thingGrid.ThingAt<Blueprint>(loc) != null) return new AcceptanceReport("Cannot be placed on blueprint.");
 			
 			//Determine other cell coords
-			IntVec3 loc2 = loc;
-			if (rot.AsInt == 0) loc2.z++;
-			else if (rot.AsInt == 1) loc2.x++;
-			else if (rot.AsInt == 2) loc2.z--;
-			else loc2.x--;
+			IntVec3 intVec2 = loc + IntVec3.North.RotatedBy(rot);
 				
-			//try to determine if this is a wall. There isn't a wall type so we check for some assumed values.
-			IEnumerable<Thing> building2 = map.thingGrid.ThingsAt(loc2);
+			//Try to determine if this is a wall. There isn't a wall type so we check for some assumed values.
+			IEnumerable<Thing> building2 = map.thingGrid.ThingsAt(intVec2);
 			foreach (Thing thinghere in building2)
 			{
-				if (thinghere?.def.thingClass == typeof(Building))
+				if (thinghere?.def.thingClass == typeof(Building) || thinghere?.def.thingClass == typeof(Mineable))
 				{
+					//Log.Message(thinghere.def.label + ", thing class:" + thinghere.def.thingClass.ToString() + "fill:" + thinghere.def.fillPercent.ToString() + ", passability:" + thinghere.def.passability.ToString() + ", blocks light:" + thinghere.def.blockLight);
 					if (thinghere != null && (thinghere.def.fillPercent == 1f || thinghere.def.passability == Traversability.Impassable || thinghere.def.blockLight == true)) return true;
 				}
 			}
